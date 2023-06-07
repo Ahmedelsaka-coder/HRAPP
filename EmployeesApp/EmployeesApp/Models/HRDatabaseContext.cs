bbc46264a1f01c173f70eeb38958ec3f6ade13d8
Ahmedelsaka-coder <ahmedelsaka6632@gmail.com>
2023-06-07 17:16:07 +0300

    HR Employee Application

---
+++ EmployeesApp/EmployeesApp/Models/HRDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeesApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace EmployeesApp.Models
+{
+    public class HRDatabaseContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Data Source=DESKTOP-8GF8PRF; Initial Catalog=EmployeesDB; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False");
+        }
+    }
+}
+    
